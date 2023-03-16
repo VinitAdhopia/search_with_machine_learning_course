aa36dc228195b70f9c39dde9c89754a7ad23cc6e
Vinit Adhopia <vinitadhopia@gmail.com>
2023-03-16 01:53:14 +0000

    Complete Level 1 Task 1

--- week3/create_labeled_queries.py
+++ week3/create_labeled_queries.py
@@ -36,10 +36,14 @@ root = tree.getroot()
 # Parse the category XML file to map each category id to its parent category id in a dataframe.
 categories = []
 parents = []
+max_depth = 0
 for child in root:
     id = child.find('id').text
     cat_path = child.find('path')
     cat_path_ids = [cat.find('id').text for cat in cat_path]
+    # Determine the longest branch in the tree
+    if len(cat_path_ids) > max_depth:
+        max_depth = len(cat_path_ids)
     leaf_id = cat_path_ids[-1]
     if leaf_id != root_category_id:
         categories.append(leaf_id)
@@ -50,9 +54,11 @@ parents_df = pd.DataFrame(list(zip(categories, parents)), columns =['category', 
 queries_df = pd.read_csv(queries_file_name)[['category', 'query']]
 queries_df = queries_df[queries_df['category'].isin(categories)]
 
+print("Normalizing...")
 # Convert queries to lower case, replace non-alphanumeric chars with a space and replace consecutive spaces with a single space
 queries_df['query'] = queries_df['query'].str.lower().replace("[^0-9a-z]+", " ", regex=True).replace("\s+", " ", regex=True)
 
+print("Stemming...")
 # For every query, tokenize the query, stem each individual token, then rejoin and update the panda
 for i, row in queries_df.iterrows():
     query = queries_df.at[i, 'query']
@@ -64,6 +70,20 @@ for i, row in queries_df.iterrows():
     queries_df.at[i,'query'] = stemmed_tokens
 
 # IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+
+print("Rolling up categories...")
+keep_looping = True
+i = 1
+# We can't roll up more times than the max depth
+while keep_looping and i < max_depth:
+    keep_looping = False
+    for category, count in queries_df['category'].value_counts().items():
+        if count < min_queries:
+            # We found at least one category that has fewer than the threshold queries so we'll need to continue looking for more
+            keep_looping = True
+            parent_category = parents_df.loc[parents_df['category'] == category, 'parent'].item()
+            queries_df.loc[queries_df['category'] == category, 'category'] = parent_category
+    i+=1
 
 # Create labels in fastText format.
 queries_df['label'] = '__label__' + queries_df['category']
