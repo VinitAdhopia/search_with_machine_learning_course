227a57f4f08e45dc534b255b26bd91e8db4ab36c
Vinit Adhopia <vinitadhopia@gmail.com>
2023-03-16 03:40:06 +0000

    Completed Level 2 Task 1.  Use our query classification model to classify user queries.

--- utilities/query.py
+++ utilities/query.py
@@ -11,11 +11,13 @@ from urllib.parse import urljoin
 import pandas as pd
 import fileinput
 import logging
-
+import fasttext
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.basicConfig(format='%(levelname)s:%(message)s')
+
+query_classification_model = "../../datasets/fasttext/queries.bin"
 
 # expects clicks and impressions to be in the row
 def create_prior_queries_from_group(
@@ -194,6 +196,10 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
 
 def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc"):
     #### W3: classify the query
+    # Load our query classification model
+    model = fasttext.load_model(query_classification_model)
+    classification = model.predict(user_query)
+    print(f"Classification: {classification}")
     #### W3: create filters and boosts
     # Note: you may also want to modify the `create_query` method above
     query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
