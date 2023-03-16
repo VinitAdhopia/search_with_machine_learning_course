69cea91c825665a315669eea9bc82fb0067cb194
Vinit Adhopia <vinitadhopia@gmail.com>
2023-03-16 15:14:27 +0000

    Filter search results based on category predictions

--- utilities/query.py
+++ utilities/query.py
@@ -17,7 +17,11 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.basicConfig(format='%(levelname)s:%(message)s')
 
+# Classication constants
 query_classification_model = "../../datasets/fasttext/queries.bin"
+# If we cannot get to a cumulative score of 0.5 with 3 predictions, these predictions probably aren't that helpful
+classification_threshold = 0.5
+num_predictions = 3
 
 # expects clicks and impressions to be in the row
 def create_prior_queries_from_group(
@@ -193,16 +197,44 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
         query_obj["_source"] = source
     return query_obj
 
-
 def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc"):
     #### W3: classify the query
     # Load our query classification model
     model = fasttext.load_model(query_classification_model)
-    classification = model.predict(user_query)
-    print(f"Classification: {classification}")
+    predictions = model.predict(user_query, num_predictions)
+    print(f"predictions: {predictions}, {predictions[0]}, {predictions[1]}, {predictions[0][0]}, {predictions[1][0]}")
+
+    category_prefix_len = len("__label__")
+    category_ids = []
+    cumulative_threshold = 0.0
+    # Accumulate category predictions until we run out of categories or meet the threshold
+    if len(predictions) >= 2:
+        categories = predictions[0]
+        scores = predictions[1]
+
+        if len(categories) > 0 and len(categories) == len(scores):
+            for idx, category in enumerate(categories):
+                category_ids.append(category[category_prefix_len:])
+                cumulative_threshold += scores[idx]
+                # Only pick enough categories to get past the threshold.  Additional categories will just add noise.
+                if cumulative_threshold >= classification_threshold:
+                    break
+                
     #### W3: create filters and boosts
+
+    filters = None
+
+    if cumulative_threshold >= classification_threshold:
+        assert len(category_ids) > 0
+
+        filters = [
+            {
+                "match" : { "categoryPathIds" : " OR ".join(category_ids)}
+            }
+        ]
+
     # Note: you may also want to modify the `create_query` method above
-    query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
+    query_obj = create_query(user_query, click_prior_query=None, filters=filters, sort=sort, sortDir=sortDir, source=["name", "shortDescription", "categoryPath", "categoryPathIds", "categoryLeaf"])
     logging.info(query_obj)
     response = client.search(query_obj, index=index)
     if response and response['hits']['hits'] and len(response['hits']['hits']) > 0:
